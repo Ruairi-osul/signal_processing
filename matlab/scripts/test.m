4a4c80360298cf4168bf3bf13cd607f8c4de5ad8
Ruairi-osul <rorylikesmusic@gmail.com>
2019-05-31 16:28:59 +0100

    added test

--- matlab/scripts/test.m
+++ matlab/scripts/test.m
@@ -5,35 +5,63 @@ load('sampleEEGdata.mat')
 
 %%
 
-fs = EEG.srate;
-t = EEG.times;
+%% short-window FFT
 
-frex = logspace(log10(10), log10(fs/5), 20);
+load sampleEEGdata.mat
 
-timewin = 300; % in ms
-timewin = round(timewin / (fs * 1000)) % in samples
+chan2plot  = 'o1';
 
-window_centres = -300:25:800;
-times_idx = dsearchn(t', window_centres');
+frex       = logspace(log10(10),log10(EEG.srate/5),20);
+times2save = -300:25:800;
+basetime   = [-300 -100];
+timewin    = 300; % in ms
 
-basetime = [-300, -100];
-base_idx = dsearchn(window_centres', basetime');
 
-hz = linspace(0, fs/2, (timewin / 2) + 1);
+% convert time points to indices
+times2saveidx = dsearchn(EEG.times',times2save'); 
+% convert time window to points
+timewinpnts   = round(timewin/(1000/EEG.srate));
 
-hanwin = 0.5 - (0.5 * cos(2*pi.*linspace(0, 1, timewin)))';
+% find baselinetimepoints
+baseidx = dsearchn(times2save',basetime');
 
-tf = zeros(length(frex), length(window_centres));
+% define frequencies for FFT
+hz = linspace(0,EEG.srate/2,timewinpnts/2+1);
 
-chan = 5;
-data = EEG.data(chan, :, :);
+% hanning window for tapering
+hannwin = .5 - .5*cos(2*pi.*linspace(0,1,timewinpnts))';
 
-for i = 1:length(window_centres)
-    time_inds = [window_centres(i) - floor(timewin / 2) + 1, ...
-                 window_centres(i) + ceil(timewin / 2)];
-    data_sub = squeeze(data(:, time_inds(1):time_inds(2), :));
-    data_sub = baxfun(@times, data_sub, hanwin);
+% find logical channel index
+chanidx = strcmpi(chan2plot,{EEG.chanlocs.labels});
 
-    data_subX = fft(data_sub) ./ timewin;
-    mean_pow = mean(abs(data_sub).^2), 2);
-end
+% initialize output matrix
+shortFFT_tf = zeros(length(frex),length(times2save));
+
+
+% loop through time bins
+for ti=1:length(times2saveidx)
+    
+    % window and taper data, and get power spectrum
+    data = bsxfun(@times, squeeze(EEG.data(chanidx,times2saveidx(ti)-floor(timewinpnts/2)+1:times2saveidx(ti)+ceil(timewinpnts/2),:)), hannwin);
+    % uncomment the next line to use non-tapered data
+    %data = squeeze(EEG.data(chanidx,times2saveidx(ti)-floor(timewinpnts/2)+1:times2saveidx(ti)+ceil(timewinpnts/2),:));
+    
+    y    = fft(data,timewinpnts)/timewinpnts;
+    pow  = mean(abs(y).^2,2);
+    
+    % finally, get power from closest frequency
+    closestfreq = dsearchn(hz',frex');
+    shortFFT_tf(:,ti) = pow(closestfreq);    
+end % end time loop
+
+% db-correct
+db_shortFFT_tf = 10*log10( bsxfun(@rdivide,shortFFT_tf,mean(shortFFT_tf(:,baseidx(1):baseidx(2)),2)) );
+
+% plot!
+figure(1), clf
+contourf(times2save,frex,db_shortFFT_tf,40,'linecolor','none')
+set(gca,'clim',[-2 2])
+xlabel('Time (ms)'), ylabel('Frequency (Hz)')
+title([ 'Power via short-window FFT (window=' num2str(timewin) ') from channel ' chan2plot ])
+
+%%
